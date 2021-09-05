626f6d9646c13024fd39ef2187d01f0febaebf60
Steve <stevesun5160@gmail.com>
2021-09-06 00:31:46 +0800

    調整Booking頁面，新增Searching頁面

--- ZoneRadar/Controllers/SearchingController.cs
+++ ZoneRadar/Controllers/SearchingController.cs
@@ -13,5 +13,10 @@ namespace ZoneRadar.Controllers
         {
             return View();
         }
+
+        public ActionResult SearchingPage()
+        {
+            return View();
+        }
     }
 }
