07d1fb6bfed2d25560d40777b642bc2ba120608c
iamamber@life.fubon.com <iamamber@life.fubon.com>
2021-09-29 19:28:02 +0800

    換Jenny的REPO

--- ZoneRadar/Repositories/zoneRadarRepository.cs
+++ ZoneRadar/Repositories/zoneRadarRepository.cs
@@ -1,35 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
-using ZoneRadar.Models;
+using ZoneRadar.Data;
 
 namespace ZoneRadar.Repositories
 {
-    /// <summary>
-    /// 新增功能中下拉選項的縣市 行政區 
-    /// </summary>
-    public class FormAreaRepository
+  
+    public class ZONERadarRepository
     {
-        private readonly ZoneRadarDB _zoneradarDB;
-        public  FormAreaRepository()
+        private readonly ZONERadarContext _context;
+        public ZONERadarRepository()
         {
-            _zoneradarDB = new ZoneRadarDB();
+            _context = new ZONERadarContext();
         }
-        public IQueryable<City> GetAllCities()
+        public IQueryable<T> GetAll<T>() where T : class
         {
-            var cities = _zoneradarDB.City;
-            return cities;
+            return _context.Set<T>();
         }
-        public IQueryable<District> GetAllDistricts()
+        public void Create<T>(T value) where T : class
         {
-            var districts = _zoneradarDB.District;
-            return districts;
+            _context.Entry(value).State = EntityState.Added;
         }
-        public IQueryable<AmenityDetail> GetAllAmenities()
+        public void CreateRange<T>(IEnumerable<T> value) where T : class
         {
-            var Amenities = _zoneradarDB.AmenityDetail;
-            return Amenities;
+            _context.Set<T>().AddRange(value);
+        }
+        public void Update<T>(T value) where T : class
+        {
+            _context.Entry(value).State = EntityState.Modified;
+        }
+        public void Delete<T>(T value) where T : class
+        {
+            _context.Entry(value).State = EntityState.Deleted;
+        }
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            _context.Dispose();
         }
     }
 }
