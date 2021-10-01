f805ea3b7dfafcbd255728df5345d048a5f87bac
Jack <n3gp62145@gmail.com>
2021-10-01 20:39:04 +0800

    Revert "新增加收藏頁面引用資料庫生產"
    This reverts commit 429a9b39a6a2ddf8350e977a5bd2c4e1542a499f.

--- ZoneRadar/Controllers/MemberCenterController.cs
+++ ZoneRadar/Controllers/MemberCenterController.cs
@@ -3,20 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using ZoneRadar.Services;
 
 namespace ZoneRadar.Controllers
 {
     public class MemberCenterController : Controller
     {
-        //private readonly MyCollectionService _myCollectionservice;
-        private readonly HostInfoService _hostinfoservice;
-        private readonly MyCollectionService _mycollectionservice;
-        public MemberCenterController()
-        {
-            _hostinfoservice = new HostInfoService();
-            _mycollectionservice = new MyCollectionService();
-        }
         // GET: MemberCenter
         public ActionResult Index()
         {
@@ -38,29 +29,17 @@ namespace ZoneRadar.Controllers
         {
             return View();
         }
-        public ActionResult MyCollection(int? memberId)
+        public ActionResult MyCollection()
         {
-            if (!memberId.HasValue) 
-            {
-                return RedirectToRoute(new { controller ="Home" , action ="Index" });
-            }
-            
-            var MemCollection = _mycollectionservice.GetMemCollection(memberId.Value);
-            
-            return View(MemCollection);
+            return View();
         }
         public ActionResult UserInfo()
         {
             return View();
         }
-        public ActionResult HostInfo(int? memberId)
+        public ActionResult HostInfo()
         {
-            if (!memberId.HasValue)
-            {
-                return RedirectToRoute(new { controller = "Home", action = "Index" });
-            }
-            var MemSpaces = _hostinfoservice.GetMemberSpace(memberId.Value);
-            return View(MemSpaces);
+            return View();
         }
     }
 }
