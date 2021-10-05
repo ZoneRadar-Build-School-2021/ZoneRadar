40146c585ad56978f282aaf40a8b0c2f9de6be30
Steve Sun <stevesun5160@gmail.com>
2021-10-05 14:14:05 +0800

    enable cors

--- ZoneRadar/Controllers/JSONAPIController.cs
+++ ZoneRadar/Controllers/JSONAPIController.cs
@@ -1,19 +1,27 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using ZoneRadar.Models;
+using ZoneRadar.Models.ViewModels;
+using ZoneRadar.Repositories;
 using ZoneRadar.Services;
 
 namespace ZoneRadar.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class JSONAPIController : ApiController
     {
         private readonly SpaceService _spaceService;
+        private readonly ZONERadarRepository _repository;
         public JSONAPIController()
         {
             _spaceService = new SpaceService();
+            _repository = new ZONERadarRepository();
         }
 
         [AcceptVerbs("GET")]
@@ -28,16 +36,21 @@ namespace ZoneRadar.Controllers
             return Ok(json);
         }
 
-        //[AcceptVerbs("POST")]
-        //public IHttpActionResult ReceiveFilterOption(string option)
-        //{
-        //    var json = _spaceService.GetFilterJSON();
-        //    if (json == null)
-        //    {
-        //        return NotFound();
-        //    }
+        [AcceptVerbs("GET", "POST")]
+        public IHttpActionResult GetFilteredSpaces(QueryViewModel query)
+        {
+            var city = query.City;
+            var district = query.District;
+            var type = query.Type;
+            var Date = query.Date;
+            var lowPrice = query.LowPrice;
+            var highPrice = query.HighPrice;
+            var amenities = query.AmenityList;
 
-        //    return Ok(json);
-        //}
+            var queriedSpaces = _repository.GetAll<Space>().Where(x => x.City.CityName == city).Select(x => x).ToList();
+            var json = JsonConvert.SerializeObject(queriedSpaces);
+
+            return Ok(json);
+        }
     }
 }
