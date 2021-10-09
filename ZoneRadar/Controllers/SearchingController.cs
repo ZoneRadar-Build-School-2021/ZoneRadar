182996de0c5bfa1f08665f587169eb769f57a92e
Steve <stevesun5160@gmail.com>
2021-10-10 01:43:52 +0800

    建立空的Controller

---
+++ ZoneRadar/Controllers/SearchingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ZoneRadar.Controllers
+{
+    public class SearchingController : Controller
+    {
+        // GET: Searching
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
