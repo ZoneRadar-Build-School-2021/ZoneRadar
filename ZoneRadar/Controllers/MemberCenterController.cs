a44bdbdc164fc9bebbf2221fa12007a67bb183c3
Steve <stevesun5160@gmail.com>
2021-10-10 00:36:34 +0800

    Revert "新增註冊/登入失敗的相符錯誤訊息、更改首頁圖片"
    This reverts commit dbfcde89f6c05b136c72d6c805134a0ba29676da.

--- ZoneRadar/Controllers/MemberCenterController.cs
+++ ZoneRadar/Controllers/MemberCenterController.cs
@@ -118,8 +118,9 @@ namespace ZoneRadar.Controllers
         public ActionResult Register()
         {
             TempData["RegisterModalPopup"] = true;
+
             //若直接輸入路由：導回原本頁面並跳出註冊Modal
-            return RedirectToAction("Index", "Home");
+            return Redirect(Request.UrlReferrer.AbsolutePath);
         }
 
         [HttpPost]
@@ -129,32 +130,25 @@ namespace ZoneRadar.Controllers
             if (!ModelState.IsValid || registerVM.Password != registerVM.ConfirmPassword)
             {
                 //輸入格式不正確或密碼不一致
-                TempData["Alert"] = true;
-                TempData["Message"] = "輸入格式不正確或密碼不一致，請重新註冊！";
-                TempData["Icon"] = false;
-                return Redirect(Request.UrlReferrer.AbsolutePath);
+                return View("Register");
             }
             else
             {
                 //先將註冊資訊存進資料庫中
-                var memberResult = _service.RegisterMember(registerVM);
+                var registerResult = _service.RegisterMember(registerVM);
                 //如果註冊資訊成功儲存
-                if (memberResult.IsSuccessful)
+                if (registerResult.IsSuccessful)
                 {
-                    //測試：用Session記錄註冊資訊
-                    //Session["ConfirmRegister"] = new List<string>() { registerResult.User.Email, DateTime.Now.AddMinutes(10).ToString() };
+                    Session["ConfirmRegister"] = new List<string>() { registerResult.User.Email, DateTime.Now.AddMinutes(10).ToString() };
                     //接著寄送驗證信
-                    _service.SentEmail(Server, Request, Url, memberResult.User.Email);
+                    _service.SentEmail(Server, Request, Url, registerResult.User.Email);
                     return View("HadSentEmail");
                 }
                 else
                 {
-                    //註冊失敗
-                    TempData["Alert"] = true;
-                    TempData["Message"] = memberResult.ShowMessage;
-                    TempData["Icon"] = memberResult.IsSuccessful;
-                    return Redirect(Request.UrlReferrer.AbsolutePath);
-                }
+                    //註冊失敗，請重新註冊
+                    return View("HadSentEmail");
+                }              
             }
         }
 
@@ -163,7 +157,7 @@ namespace ZoneRadar.Controllers
             //測試：Session是否接收的到資料？(答：收不到，是null)
             //var test = (List<string>)Session["ConfirmRegister"];
             //var xxx = Request.Cookies["ConfirmRegister"].Value;
-
+            
             var expiredTime = new DateTime();
             DateTime.TryParse(expired, out expiredTime);
             //超過10分鐘無效
@@ -172,28 +166,21 @@ namespace ZoneRadar.Controllers
                 return View();
             }
             //確認是否有此註冊資訊
-            var memberResult = _service.ConfirmRegister(email);
+            var registerResult = _service.ConfirmRegister(email);
 
             //註冊成功
-            if (memberResult.IsSuccessful)
+            if (registerResult.IsSuccessful)
             {
                 //讓使用者登入，呈現登入後的畫面
-                var encryptedTicket = _service.CreateEncryptedTicket(memberResult.User);
+                var encryptedTicket = _service.CreateEncryptedTicket(registerResult.User);
                 _service.CreateCookie(encryptedTicket, Response);
                 //導回原本的畫面
-                var originalUrl = _service.GetOriginalUrl(memberResult.User.MemberID.ToString());
-
-                TempData["Alert"] = true;
-                TempData["Message"] = memberResult.ShowMessage;
-                TempData["Icon"] = memberResult.IsSuccessful;
+                var originalUrl = _service.GetOriginalUrl(registerResult.User.MemberID.ToString());
                 return Redirect(originalUrl);
             }
             else
             {
-                //註冊失敗，回去首頁，跳出錯誤訊息
-                TempData["Alert"] = true;
-                TempData["Message"] = memberResult.ShowMessage;
-                TempData["Icon"] = memberResult.IsSuccessful;
+                //註冊失敗，回去註冊畫面，跳出錯誤訊息(還沒寫)
                 return RedirectToAction("Index", "Home");
             }
         }
@@ -211,7 +198,7 @@ namespace ZoneRadar.Controllers
             }
 
             //若直接輸入路由：導回原本頁面並跳出登入Modal
-            return RedirectToAction("Index", "Home");
+            return Redirect(Request.UrlReferrer.AbsolutePath);
         }
 
         [HttpPost]
@@ -221,40 +208,34 @@ namespace ZoneRadar.Controllers
             //若未通過Model驗證(前端已先驗證過)
             if (!ModelState.IsValid)
             {
-                //回到原本頁面並跳出錯誤訊息
-                TempData["Alert"] = true;
-                TempData["Message"] = "輸入格式不正確，請重新註冊！";
-                TempData["Icon"] = false;
-                return Redirect(Request.UrlReferrer.AbsolutePath);
+                //回到原本頁面並跳出登入Modal，跳出錯誤訊息(還沒做)
+                TempData["LoginModalPopup"] = true;
+                Response.Redirect(Request.UrlReferrer.AbsolutePath);
+                //return View();
             }
 
-            var memberResult = _service.UserLogin(loginVM);
+            var user = _service.UserLogin(loginVM);
                        
-            //找不到此會員
-            if (memberResult.User == null)
+            //找不到則彈回Login頁
+            if (user == null)
             {
-                TempData["Alert"] = true;
-                TempData["Message"] = memberResult.ShowMessage;
-                TempData["Icon"] = memberResult.IsSuccessful;
-                //TempData["Email"] = loginVM.Email;
+                TempData["LoginModalPopup"] = true;
+                TempData["Email"] = loginVM.Email;
                 //TempData["Email"] = ModelState["LoginZONERadarVM.Email"];
-
-                //回到原本頁面並跳出錯誤訊息
+                
+                //回到原本頁面並跳出登入Modal
                 return Redirect(Request.UrlReferrer.AbsolutePath);
             }
 
             //建造加密表單驗證票證
-            var encryptedTicket = _service.CreateEncryptedTicket(memberResult.User);
+            var encryptedTicket = _service.CreateEncryptedTicket(user);
 
             //建造cookie
             _service.CreateCookie(encryptedTicket, Response);
 
             //導向使用者原先欲造訪的路由
-            var originalUrl = _service.GetOriginalUrl(memberResult.User.MemberID.ToString());
+            var originalUrl = _service.GetOriginalUrl(user.MemberID.ToString());
 
-            TempData["Alert"] = true;
-            TempData["Message"] = memberResult.ShowMessage;
-            TempData["Icon"] = memberResult.IsSuccessful;
             return Redirect(originalUrl);
         }
 
