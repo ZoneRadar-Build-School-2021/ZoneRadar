092070b677dbf024b2b6366b111772bdfe116e17
Steve <stevesun5160@gmail.com>
2021-10-10 14:56:18 +0800

    加入JSON字串

--- ZoneRadar/Repositories/ZONERadarRepository.cs
+++ ZoneRadar/Repositories/ZONERadarRepository.cs
@@ -14,30 +14,37 @@ namespace ZoneRadar.Repositories
         {
             _context = new ZONERadarContext();
         }
+
         public IQueryable<T> GetAll<T>() where T : class
         {
             return _context.Set<T>();
         }
+
         public void Create<T>(T value) where T : class
         {
             _context.Entry(value).State = EntityState.Added;
         }
+
         public void CreateRange<T>(IEnumerable<T> value) where T : class
         {
             _context.Set<T>().AddRange(value);
         }
+
         public void Update<T>(T value) where T : class
         {
             _context.Entry(value).State = EntityState.Modified;
         }
+
         public void Delete<T>(T value) where T : class
         {
             _context.Entry(value).State = EntityState.Deleted;
         }
+
         public void SaveChanges()
         {
             _context.SaveChanges();
         }
+
         public void Dispose()
         {
             _context.Dispose();
