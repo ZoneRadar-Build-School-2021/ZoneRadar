9d6660a762673ce023ce226aa3c0d245f091e83c
MTHuang <siksj3@gmail.com>
2021-10-10 19:36:49 +0800

    更新homepage和layout的Assets資料夾資料

--- ZoneRadar/Assets/JS/layout/navbar-footer.js
+++ ZoneRadar/Assets/JS/layout/navbar-footer.js
@@ -9,11 +9,4 @@ window.onload = () => {
         bootstrap.Modal.getOrCreateInstance(login_modal).hide();
     })
     //#endregion
-
-    //#region 導覽列的成為場地主登入前後連結更改
-    // let become_host_link = document.querySelector("nav.shared-navbar .become-host-link");
-    // become_host_link.setAttribute("href", "~/MemberCenter/SellerCenter");
-    // become_host_link.removeAttribute("data-bs-toggle");
-    // become_host_link.removeAttribute("data-bs-target");
-    //#endregion
 }
