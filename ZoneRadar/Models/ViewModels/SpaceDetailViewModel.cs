ece5ddd2499584e57ca4895e071c77e6e1a71580
Steve <stevesun5160@gmail.com>
2021-10-10 16:42:49 +0800

    分離bookingpageviewmodel

---
+++ ZoneRadar/Models/ViewModels/SpaceDetailViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZoneRadar.Models.ViewModels
+{
+    public class SpaceDetailViewModel
+    {
+        public string HostName { get; set; }
+        public string HostPhoto { get; set; }
+        public Dictionary<string, List<string>> CleaningOptionDict { get; set; }
+        public string Introduction { get; set; }
+        public string ShootingEquipment { get; set; }
+        public string ParkingInfo { get; set; }
+        public string HostRule { get; set; }
+        public Dictionary<string, List<string>> AmenityDictionary { get; set; }
+        public string Longitude { get; set; }
+        public string Latitude { get; set; }
+        public string TrafficInfo { get; set; }
+        public List<string> OperatingDayList { get; set; }
+        public List<TimeSpan> StartTimeList { get; set; }
+        public List<TimeSpan> EndTimeList { get; set; }
+        public string CancellationTitle { get; set; }
+        public string CancellationInfo { get; set; }
+        public int HoursForDiscount { get; set; }
+        public Decimal Discount { get; set; }
+    }
+}
---
+++ ZoneRadar/Models/ViewModels/SpaceReviewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZoneRadar.Models.ViewModels
+{
+    public class SpaceReviewViewModel
+    {
+        public int Score { get; set; }
+        public string ReviewContent { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public bool IsRecommend { get; set; }
+        public string ReviewedMemberName { get; set; }
+        public string ReviewedMemberPhoto { get; set; }
+    }
+}
