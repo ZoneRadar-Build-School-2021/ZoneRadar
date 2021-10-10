1dcbec564f9dc26ca1482bf831272f51c7e24f61
Steve <stevesun5160@gmail.com>
2021-10-10 14:45:19 +0800

    複製貼上Searching Controller

--- ZoneRadar/Controllers/SearchingController.cs
+++ ZoneRadar/Controllers/SearchingController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZoneRadar.Services;
 
 namespace ZoneRadar.Controllers
 {
     public class SearchingController : Controller
     {
-        // GET: Searching
-        public ActionResult Index()
+        private readonly SpaceService _spaceService;
+        public SearchingController()
+        {
+            _spaceService = new SpaceService();
+        }
+
+        /// <summary>
+        /// 搜尋頁(Steve)
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult SearchingPage()
         {
             return View();
         }
+
     }
 }
