ea195eac2c1ae07ad17bcd34b444ce13cee24dc8
iamamber@life.fubon.com <iamamber@life.fubon.com>
2021-10-11 18:09:45 +0800

    引用命名空間

--- ZoneRadar/Controllers/HostCenterController.cs
+++ ZoneRadar/Controllers/HostCenterController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZoneRadar.Models.ViewModels;
+using ZoneRadar.Services;
+
 
 namespace ZoneRadar.Controllers
 {
