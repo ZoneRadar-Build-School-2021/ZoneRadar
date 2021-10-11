fa04f1fde040825b7a3c7684f703d5653576f3e7
Nick <m0935716520@gmail.com>
2021-10-11 10:25:43 +0800

    加入 預購單 + 訂單狀態 ViewModel

---
+++ ZoneRadar/Models/ViewModels/ShopCarViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ZoneRadar.Models.ViewModels
+{
+    public class ShopCarViewModel
+    {
+        public string SpaceName { get; set; }
+        public string SpaceUrl { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerPhone { get; set; }
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public decimal Money { get; set; }
+        public int OrderID { get; set; }
+        public List<RentDetailViewModel> RentDetailVM { get; set; }
+    }
+
+    public class RentDetailViewModel
+    {
+        public string RentTime { get; set; }
+        public string RentBackTime { get; set; }
+        public int People { get; set; }
+        public int OrderDetailId { get; set; }
+        public int OrderId { get; set; }
+    }
+}
---
+++ ZoneRadar/Models/ViewModels/UsercenterProcessingViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ZoneRadar.Models.ViewModels
+{
+    public class UsercenterProcessingViewModel
+    {
+        public int OrderNumber { get; set; }
+        public DateTime PaidTime { get; set; }
+        public string SpaceName { get; set; }
+        public string SpaceUrl { get; set; }
+        public int Score { get; set; }
+        public DateTime RentTime { get; set; }
+        public DateTime RentBackTime { get; set; }
+        public int People { get; set; }
+        [DisplayFormat(DataFormatString = "{0:N0}")]
+        public decimal Money { get; set; }
+    }
+}
