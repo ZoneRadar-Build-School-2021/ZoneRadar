15258f38956e59bc93915afb64a247f0cd1ef54f
iamamber@life.fubon.com <iamamber@life.fubon.com>
2021-10-13 07:06:20 +0800

    增加SpaceViewModel

---
+++ ZoneRadar/Models/ViewModels/SpaceViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ZoneRadar.Models.ViewModels
+{
+    public class SpaceViewModel
+    {
+    }
+}
