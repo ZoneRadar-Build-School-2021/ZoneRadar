aa8499ee8378a42997d4471028d3586a3978a8cf
Jack <n3gp62145@gmail.com>
2021-10-12 18:07:02 +0800

    修改Member CSS內容 , ViewModel內容

--- ZoneRadar/Models/ViewModels/HostInfoViewModel.cs
+++ ZoneRadar/Models/ViewModels/HostInfoViewModel.cs
@@ -12,6 +12,7 @@ namespace ZoneRadar.Models.ViewModels
     }
     public class User 
     {
+        public int Id { get; set; }
         public string Email { get; set; }
 
         public string Password { get; set; }
