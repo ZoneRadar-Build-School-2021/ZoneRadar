0481479ff3061e8dcfcca2cafdf139fa738835e0
iamamber@life.fubon.com <iamamber@life.fubon.com>
2021-10-13 07:04:02 +0800

    修改命名空間

--- ZoneRadar/Controllers/HostCenterController.cs
+++ ZoneRadar/Controllers/HostCenterController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ZoneRadar.Services;
+using ZoneRadar.Models;
 
 namespace ZoneRadar.Controllers
 {
