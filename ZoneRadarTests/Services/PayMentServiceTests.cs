033d49a63525a8b7687a07915a9b27cdb38b5216
Nick <m0935716520@gmail.com>
2021-10-25 21:20:59 +0800

    新增單元測試

---
+++ ZoneRadarTests/Services/PayMentServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZoneRadar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZoneRadar.Services.Tests
+{
+    [TestClass()]
+    public class PayMentServiceTests
+    {
+        [TestMethod()]
+        public void Given_EndTime_202111052000_StartTime_202111050900_HourPrice_120_DiscountHour_8_Discount_085_When_CalculateMoney_Then_1122()
+        {
+            decimal price = 1122;
+            
+        }
+    }
+}
