ef47e170eee19c47a983f7e5061a5b4f2b84b4a9
Nick <m0935716520@gmail.com>
2021-10-26 22:01:42 +0800

    修改時間格式

--- ZoneRadar/Assets/JS/order_state/usercenter_shopcar.js
+++ ZoneRadar/Assets/JS/order_state/usercenter_shopcar.js
@@ -2,13 +2,13 @@ flatpickr.localize(flatpickr.l10ns.zh_tw);
 flatpickr("#basicDate_rent", {
   enableTime: true,
   minDate: "today",
-  dateFormat: "Y-m-d H:i",
+  dateFormat: "Y/m/d H:i",
   minuteIncrement: 30,
 });
 
 flatpickr("#basicDate_rentback", {
   enableTime: true,
   minDate: "today",
-  dateFormat: "Y-m-d H:i",
+  dateFormat: "Y/m/d H:i",
   minuteIncrement: 30,
 });
