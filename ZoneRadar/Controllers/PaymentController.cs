946a66e3b3f0f8d1c4f6ae8d608496d333843a29
Jack <n3gp62145@gmail.com>
2021-11-01 19:17:26 +0800

    Services

--- ZoneRadar/Controllers/PaymentController.cs
+++ ZoneRadar/Controllers/PaymentController.cs
@@ -25,35 +25,7 @@ namespace ZoneRadar.Controllers
         /// <returns></returns>
         public ActionResult Payment(CartsViewModel model)
         {
-            var ReturnURL = "http://www.ecpay.com.tw/";
-            var ClientBackURL = "https://localhost:44322/UserCenter/Pending";
-            var MerchantTradeNo = "ECPay" + new Random().Next(0, 999).ToString() + DateTime.UtcNow.ToString("yyyyMMddHHmm");
-            var MerchantradeDate = DateTime.UtcNow.ToString("yyyy/MM/dd HH:mm:ss");
-            
-
-
-            var order = new Dictionary<string, string>
-            {
-                var Model = _ecpaymentservice.GetPaymentData(model.OrderId);
-                ViewData["OrderId"] = model.OrderId;
-                ViewData["SpaceName"] = model.SpaceName;
-                ViewData["TotalMoney"] = model.TotalMoney;
-
-                return View(Model);
-            }
-            else 
-            {
-                return RedirectToRoute(new { controller = "Home", action = "Index" });
-            }
-            //if (!memberId.HasValue)
-            //{
-            //    return RedirectToRoute(new { controller = "Home", action = "Index" });
-            //}
-            //else
-            //{
-            //    var MemCollectionSpaces = _service.GetMemberCollection(memberId.Value);
-            //    return View(MemCollectionSpaces);
-            //}
+            return View();
         }
 
         public ActionResult EcPayment(CartsViewModel model) 
