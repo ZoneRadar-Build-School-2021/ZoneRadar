72911d6e79018e54f17052ac1febe12a58e5a7bb
Abcdefg <abc56494@gmail.com>
2021-11-04 03:22:58 +0800

    增加編輯頁面按鈕功能+Vue.js in Layout

--- ZoneRadar/Assets/JS/membercenter/profile.js
+++ ZoneRadar/Assets/JS/membercenter/profile.js
@@ -1,64 +1,34 @@
-﻿//photo upload
-var CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/bs20210831ta/upload';
-var CLOUDINARY_UPLOAD_PRESET = 'hblvplct';
-
-var imgview = document.getElementById('imgpic');
-var fileupload = document.getElementById('files');
-
-document.getElementById('upload').addEventListener('click', openDialog);
-function openDialog() {
-    fileupload.click();
-    // document.getElementById('fileid').click();
-}
-
-fileupload.addEventListener('change', function (event) {
-    var file = event.target.files[0];
-    var formData = new FormData();
-    formData.append('file', file);
-    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
-
-    axios({
-        url: CLOUDINARY_URL,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: formData
-    }).then(function (res) {
-        console.log(res);
-        imgview.src = res.data.secure_url;
-    }).catch(function (err) {
-        console.error(err);
-    });
-});
-
-//button disabled
+﻿//button disabled
 let app = new Vue({
     el: '#app',
     data: {
         isDisabled: true,
         inputData: {
-            Name: '',
-            Phone: '',
-            Description: '',
-            Checked: ''
+            Photo: profileData.Photo,
+            Name: profileData.Name,
+            Phone: profileData.Phone,
+            Email: profileData.Email,
+            Description: profileData.Description,
+            ReceiveEDM: profileData.ReceiveEDM
         },
         inputDataCheck: {
             NameError: false,
             NameErrorMsg: ''
         },
         originalinputData: {
-            Name: '',
-            Phone: '',
-            Description: '',
-            Checked: ''
+            Photo: profileData.Photo,
+            Name: profileData.Name,
+            Phone: profileData.Phone,
+            Email: profileData.Email,
+            Description: profileData.Description,
+            ReceiveEDM: profileData.ReceiveEDM
         },
     },
     watch: {
         inputData: {
             deep: true,
             handler() {
-                if (this.originalinputData.Name === this.inputData.Name && this.originalinputData.Phone === this.inputData.Phone && this.originalinputData.Description === this.inputData.Description && this.originalinputData.Checked === this.inputData.Checked) {
+                if (this.originalinputData.Photo === this.inputData.Photo && this.originalinputData.Name === this.inputData.Name && this.originalinputData.Phone === this.inputData.Phone && this.originalinputData.Description === this.inputData.Description && this.originalinputData.ReceiveEDM === this.inputData.ReceiveEDM) {
                     this.isDisabled = true;
                 }
                 else if (this.inputData.Name === '') {
@@ -69,7 +39,7 @@ let app = new Vue({
                 }
             }
         },
-        'inputData.name': {
+        'inputData.Name': {
             handler() {
                 if (this.inputData.Name == '') {
                     this.inputDataCheck.NameError = true;
